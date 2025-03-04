0dd2e8cd39340ea7803a0076735692f1026f3ceb
HellperKK <baudetad@gmail.com>
2025-03-04 15:00:39 +0100

    first try

---
+++ engine.js
@@ -0,0 +1,119 @@
+const signals = [];
+
+const $state = new Proxy(
+  {},
+  {
+    get: (target, key) => target[key] || "",
+    set: (target, key, value) => {
+      for (const signal of signals) {
+        if (signal.dependencies.includes(key)) {
+          signal.callback(value);
+        }
+      }
+      target[key] = value;
+      return true;
+    },
+  }
+);
+
+class EngineStory extends HTMLDivElement {
+  constructor() {
+    super();
+
+    this.startPage = this.querySelector("story-scene[start]");
+    this.startPage.activate();
+    this.currentPage = this.startPage;
+
+    this.allPages = this.querySelectorAll("story-scene");
+    this.allPages.forEach((page) => {
+      page.connect((id) => {
+        this.next(id);
+      });
+    });
+  }
+
+  next(id) {
+    const nextPage = this.querySelector(`story-scene[page-id="${id}"]`);
+    this.currentPage.deactivate();
+    nextPage.activate();
+    this.currentPage = nextPage;
+  }
+}
+
+class EnginePage extends HTMLDivElement {
+  constructor() {
+    super();
+  }
+
+  activate() {
+    this.classList.add("active");
+  }
+
+  deactivate() {
+    this.classList.remove("active");
+  }
+
+  connect(callback) {
+    this.querySelectorAll("story-choice").forEach((link) => {
+      link.connect(callback);
+    });
+  }
+}
+
+class EngineLink extends HTMLButtonElement {
+  constructor() {
+    super();
+  }
+
+  connect(callback) {
+    this.addEventListener("click", () => {
+      callback(this.getAttribute("to"));
+    });
+  }
+}
+
+class EngineData extends HTMLSpanElement {
+  constructor() {
+    super();
+    this.innerText = $state[this.getAttribute("name")];
+    signals.push({
+      dependencies: [this.getAttribute("name")],
+      callback: (value) => {
+        this.innerText = value;
+      },
+    });
+  }
+}
+
+class EngineCategory extends HTMLDivElement {
+  constructor() {
+    super();
+  }
+}
+
+class EngineInput extends HTMLElement {
+  constructor() {
+    super();
+    this.input = document.createElement("input");
+    this.input.type = this.getAttribute("type");
+    this.input.placeholder = this.getAttribute("placeholder");
+
+    const name = this.getAttribute("name");
+    this.input.value = $state[name] || "";
+    this.input.addEventListener("input", (event) => {
+      $state[name] = event.target.value;
+    });
+
+    this.appendChild(this.input);
+  }
+
+  connectedCallback() {
+  }
+}
+
+customElements.define("story-input", EngineInput);
+customElements.define("story-data", EngineData, { extends: "span" });
+customElements.define("story-choice", EngineLink, { extends: "button" });
+customElements.define("story-scene", EnginePage, { extends: "div" });
+customElements.define("story-category", EngineCategory, { extends: "div" });
+customElements.define("story-root", EngineStory, { extends: "div" });
