ad0d16c9f48aa54aa5cb3d45b5d2098b03a070e6
Adrien Baudet <baudetad@gmail.com>
2026-05-05 15:36:06 +0200

    Update engine-lite.js

--- engine-lite.js
+++ engine-lite.js
@@ -1,172 +1,145 @@
 const signals = [];
 const computeds = [];
-const events = []
+const events = [];
 
 const addSignal = (dependencies, callback) =>
-    signals.push({ dependencies, callback });
+	signals.push({ dependencies, callback });
 const addComputed = (dependencies, callback, name) =>
-    computeds.push({ dependencies, callback, name });
-const addEvent = (condition, callback) =>
-    events.push({ condition, callback });
+	computeds.push({ dependencies, callback, name });
+const addEvent = (condition, callback) => events.push({ condition, callback });
 
-let $ = new Proxy(
-    {},
-    {
-        get: (target, key) => {
-            return target[key];
-        },
-        set: (target, key, value) => {
-            target[key] = value;
+const $ = new Proxy(
+	{},
+	{
+		get: (target, key) => {
+			return target[key];
+		},
+		set: (target, key, value) => {
+			target[key] = value;
 
-            for (const signal of signals) {
-                if (signal.dependencies.some(k => key === k)) {
-                    signal.callback($[key]);
-                }
-            }
+			for (const signal of signals) {
+				if (signal.dependencies.some((k) => key === k)) {
+					signal.callback($[key]);
+				}
+			}
 
-            for (const computed of computeds) {
-                if (computed.dependencies.some(k => key === k)) {
-                    const values = computed.dependencies.map((dep) => $[dep]);
-                    $[computed.name] = computed.callback(...values);
-                }
-            }
+			for (const computed of computeds) {
+				if (computed.dependencies.some((k) => key === k)) {
+					const values = computed.dependencies.map((dep) => $[dep]);
+					$[computed.name] = computed.callback(...values);
+				}
+			}
 
-            return true;
-        },
-    }
+			return true;
+		},
+	},
 );
 
-class ERoot extends HTMLDivElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        game = this;
-        this.currentPage = this.querySelector("story-scene[start]");
-        this.currentPage.activate();
-        this.id = this.currentPage.getAttribute("pageid");
+class ERoot extends HTMLElement {
+	connectedCallback() {
+		game = this;
+		this.currentPage = this.querySelector("story-scene[start]");
+		this.currentPage.activate();
+		this.id = this.currentPage.getAttribute("pageid");
 
-        this.querySelectorAll("story-scene").forEach((page) => {
-            page.connect((id) => this.next(id))
-        });
-    }
+		this.querySelectorAll("story-scene").forEach((page) => {
+			page.connect((id) => this.next(id));
+		});
+	}
 
-    next(id, trigger = true) {
-        const nextPage = this.querySelector(`story-scene[page-id="${id}"]`);
+	next(id, trigger = true) {
+		const nextPage = this.querySelector(`story-scene[page-id="${id}"]`);
 
-        if (!nextPage) {
-            alert(`No page with id ${id}`);
-            return;
-        }
+		if (!nextPage) {
+			alert(`No page with id ${id}`);
+			return;
+		}
 
-        this.currentPage.setActive(false);
-        nextPage.setActive(true);
-        this.currentPage = nextPage;
+		this.currentPage.setActive(false);
+		nextPage.setActive(true);
+		this.currentPage = nextPage;
 
-        if (trigger) {
-            for (const event of events) {
-                if (event.condition()) {
-                    event.callback(id, this)
-                }
-            }
-        }
-    }
+		if (trigger) {
+			for (const event of events) {
+				if (event.condition()) {
+					event.callback(id, this);
+				}
+			}
+		}
+	}
 }
 
-class EScene extends HTMLDivElement {
-    constructor() {
-        super();
-    }
+class EScene extends HTMLElement {
+	setActive(flag) {
+		this.classList.toggle("active", flag);
+	}
 
-    setActive(flag) {
-        this.classList.toggle("active", flag);
-    }
-
-    connect(callback) {
-        this.querySelectorAll("story-choice").forEach((link) => {
-            link.connect(callback);
-        });
-    }
+	connect(callback) {
+		this.querySelectorAll("story-choice").forEach((link) => {
+			link.connect(callback);
+		});
+	}
 }
 
-class EChoice extends HTMLButtonElement {
-    constructor() {
-        super();
-    }
+class EChoice extends HTMLElement {
+	connect(callback) {
+		this.onclick = () => callback(this.getAttribute("to"));
+	}
 
-    connect(callback) {
-        this.onclick = () => callback(this.getAttribute("to"));
-    }
-
-    connectedCallback() {
-        const button = document.createElement("button");
-        button.innerText = this.innerText;
-        this.innerText = "";
-        this.appendChild(button);
-    }
+	connectedCallback() {
+		const button = document.createElement("button");
+		button.innerText = this.innerText;
+		this.innerText = "";
+		this.appendChild(button);
+	}
 }
 
-class EData extends HTMLSpanElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        const name = this.getAttribute("name");
+class EData extends HTMLElement {
+	connectedCallback() {
+		const name = this.getAttribute("name");
 
-        this.innerText = $[name];
-        addSignal([name],
-            (value) => {
-                this.innerText = value;
-            },
-        );
-    }
+		this.innerText = $[name];
+		addSignal([name], (value) => {
+			this.innerText = value;
+		});
+	}
 }
 
-class EIf extends HTMLSpanElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        const iff = this.getAttribute("if");
-        this.classList.toggle("hidden", $[iff]);
+class EIf extends HTMLElement {
+	connectedCallback() {
+		const iff = this.getAttribute("if");
+		this.classList.toggle("hidden", $[iff]);
 
-        addSignal([iff],
-            (value) => {
-                this.classList.toggle("hidden", value)
-            },
-        );
-    }
+		addSignal([iff], (value) => {
+			this.classList.toggle("hidden", value);
+		});
+	}
 }
 
 class EInput extends HTMLElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        const input = document.createElement("input");
-        input.type = this.getAttribute("type");
-        input.placeholder = this.getAttribute("placeholder") ?? "";
+	connectedCallback() {
+		const input = document.createElement("input");
+		input.type = this.getAttribute("type");
+		input.placeholder = this.getAttribute("placeholder") ?? "";
 
-        const name = this.getAttribute("name");
-        input.value = $[name];
-        input.oninput = (event) => set($, name, event.target.value);
+		const name = this.getAttribute("name");
+		input.value = $[name];
+		input.oninput = (event) => set($, name, event.target.value);
 
-        addSignal([name],
-            (value) => {
-                input.value = value;
-            },
-        );
+		addSignal([name], (value) => {
+			input.value = value;
+		});
 
-        this.appendChild(input);
-    }
+		this.appendChild(input);
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const c = customElements;
-    c.define("s-input", EInput);
-    c.define("s-if", EIf, { extends: "span" });
-    c.define("s-data", EData, { extends: "span" });
-    c.define("s-choice", EChoice, { extends: "button" });
-    c.define("s-scene", EScene, { extends: "div" });
-    c.define("s-root", ERoot, { extends: "div" });
-})
+	const c = customElements;
+	c.define("s-input", EInput);
+	c.define("s-if", EIf);
+	c.define("s-data", EData);
+	c.define("s-choice", EChoice);
+	c.define("s-scene", EScene);
+	c.define("s-root", ERoot);
+});
